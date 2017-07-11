0804f31b155c218620d243c7caef310e5319b6cb
travisgerrard <travisgerrard@gmail.com>
2017-07-10 21:01:56 -0700

    basic routs to signup and home

--- alclient/src/App.js
+++ alclient/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
+import Greetings from './Greetings';
+import SignUpPage from './signup/SignUpPage';
 import './App.css';
+
+const ActiveLink = ({ label, to, activeOnlyWhenExact }) => (
+  <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => (
+    <Link className={match ? 'active item' : 'item'} to={to}>{label}</Link>
+  )} />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="ui container">
+        <div className="ui mini menu">
+          <ActiveLink activeOnlyWhenExact to="/" label="Red Dice" />
+          <div className="right menu">
+            <ActiveLink activeOnlyWhenExact to="/signup" label="Sign Up" />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Route exact path="/" component={Greetings} />
+        <Route path="/signup" component={SignUpPage} />
+
       </div>
     );
   }
---
+++ alclient/src/signup/SignUpPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SignUpPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Sign up page</h1>
+      </div>
+    );
+  }
+
+}
+
+export default SignUpPage;
