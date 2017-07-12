5ff5217cee4a82896a5f64420e1a398488e245db
travisgerrard <travisgerrard@gmail.com>
2017-07-12 09:14:37 -0700

    Validate username and email uniqueness on server

--- alserver/routes/users.js
+++ alserver/routes/users.js
@@ -1,29 +1,58 @@
 const express = require('express');
 const router = new express.Router();
-import validateInput from '../validations/signup';
+import commonValidations from '../validations/signup';
 import bcrypt from 'bcrypt';
 const config = require('../config/index.json');
 const User = require('mongoose').model('User');
+import Promise from 'bluebird';
+import isEmpty from 'lodash/isEmpty';
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+
+  // quries the data base to check for existence of username and email
+  // warpped in a promise so that queries can occur and then answer passed back to client
+  return Promise.all([
+    User.count({username: data.username}, function (err, count) {
+      if(count > 0){
+        { errors.username = 'There is a user with that username already'}
+        //username already exists
+      }
+    }),
+
+    User.count({email: data.email}, function (err, count) {
+      if(count > 0){
+        { errors.email = 'There is a user with that email already'}
+        //email already exists
+      }
+    })
+  ]).then(() => {
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    }
+  });
+
+}
 
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
-
-  if (isValid) {
-    const { username, email, password } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
-    const user = User({ username, email, password_digest });
-    console.log("Adding user with req body:", { username, email, password_digest });
-    user.save((err, savedUser) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ errors: {global: "something went wrong"} });
-      }
-      return res.json({ success: true });
-    });
-  } else {
-    res.status(400).json({ errors });
-  }
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { username, email, password } = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
+      const user = User({ username, email, password_digest });
+      console.log("Adding user with req body:", { username, email, password_digest });
+      user.save((err, savedUser) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ errors: {global: "something went wrong"} });
+        }
+        return res.json({ success: true });
+      });
+    } else {
+      res.status(400).json({ errors });
+    }
+  });
 });
 
 module.exports = router;
